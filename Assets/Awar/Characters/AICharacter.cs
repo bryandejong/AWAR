7085c698306fdfdc7bcbfbaf71fa17290a58827b
bryandejong <bdjong1@avans.nl>
2019-06-27 10:49:19 +0200

    Character, AI charcter and basic working navmesh agent

---
+++ Assets/Awar/Characters/AICharacter.cs
@@ -0,0 +1,39 @@
+﻿using Awar.Utils;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Awar.Characters
+{
+    public class AICharacter : Character
+    {
+        [SerializeField] private NavMeshAgent _agent = default;
+
+        public void Start()
+        {
+            _agent.updateRotation = true;
+        }
+
+        //Test code, remove when Awar's Tick() is implemented
+        public void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = MouseControl.MouseRay();
+
+                if (Physics.Raycast(ray, out var hit, 150))
+                {
+                    Debug.Log(hit.transform.gameObject);
+                    SetTarget(hit.point);
+                }
+            }
+        }
+
+        private void SetTarget(Vector3 targetPos)
+        {
+            if(_agent.SetDestination(targetPos))
+            {
+                Move(_agent.steeringTarget);
+            };
+        }
+    }
+}
---
+++ Assets/Awar/Characters/Character.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Awar.Characters
+{
+    public class Character : AwarObject
+    {
+        [SerializeField] private Animator _animator = default;
+
+        protected bool IsMoving = false;
+
+        public override void Initialize()
+        {
+        }
+
+        public override void Tick()
+        {
+        }
+
+        public void Move(Vector3 direction)
+        {
+            _animator.SetBool("isMoving", true);
+            IsMoving = true;
+        }
+
+        public void StopMoving()
+        {
+            _animator.SetBool("isMoving", false);
+            IsMoving = false;
+        }
+    }
+}
